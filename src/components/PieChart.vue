dd457fbce734865623711fc43e6ecf2dfb241e0f
heroyooki <heroyooki@gmail.com>
2023-07-23 14:48:45 +0300

    SSE processed

---
+++ src/components/PieChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card elevation="0">
+    <g-chart
+      type="PieChart"
+      :resizeDebounce="500"
+      :options="options"
+      :data="collectionData"
+    ></g-chart>
+  </v-card>
+</template>
+
+<script>
+import { GChart } from "vue-google-charts";
+
+export default {
+  name: "PieChart",
+  components: { GChart },
+  props: {
+    collectionData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    }
+  }
+};
+</script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,6 +1,64 @@
 <template>
-  <div>Dashboard</div>
+  <pie-chart
+    :collection-data="data"
+    :options="options"
+  ></pie-chart>
 </template>
 
 <script setup>
+import {onMounted, ref} from "vue";
+import PieChart from "@/components/PieChart";
+
+const pieConf = {
+  "available": '#ea9191',
+  "charging": '#66DA26',
+  "reserved": '#2edbfa',
+  "offline": '#cfd2ce'
+}
+
+const chartHeaders = ['Status', 'Count']
+
+const data = ref([]);
+
+const options = {
+  pieHole: 0.4,
+  width: 400,
+  height: 300,
+  colors: Object.values(pieConf),
+  legend: 'none',
+};
+
+const processSSE = (event) => {
+    let response = JSON.parse(event.data)
+    let data = response.data.meta.count;
+    updatePieChart(pieConf, chartHeaders, data)
+  }
+
+// component
+const updatePieChart = (conf, headers, statuses) => {
+    data.value = [headers]
+    for (let pkey in conf) {
+      for (let rkey in statuses) {
+        if (pkey === rkey) {
+          data.value.push([pkey, statuses[rkey]])
+        }
+      }
+    }
+  }
+
+onMounted(() => {
+  // component
+  const sse = new EventSource("http://localhost:8000/stream");
+    sse.addEventListener("message", (e) => {
+      processSSE(e);
+    });
+
+  let response = {
+    "available": 0,
+    "offline": 2,
+    "reserved": 0,
+    "charging": 0
+  };
+  updatePieChart(pieConf, chartHeaders, response)
+})
 </script>
