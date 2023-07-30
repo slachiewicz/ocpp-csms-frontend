9321a3902df49f8fb1d49562a279e36aa0ae9d36
heroyooki <heroyooki@gmail.com>
2023-07-30 09:05:18 +0300

    Removed redundant providers

--- src/main.js
+++ src/main.js
@@ -16,21 +16,9 @@ import { registerPlugins } from '@/plugins'
 // Vuetify
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { createPinia } from 'pinia'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-const pinia = createPinia()
 
 const app = createApp(App)
 
 registerPlugins(app)
 
-app.use(pinia)
-app.use(vuetify)
 app.mount('#app')
